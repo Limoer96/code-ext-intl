8ae17753ba703fcac5ea93368ed638fc7aeb335f
Limoer96 <myt19960503@outlook.com>
2020-05-25 15:00:34 +0800

    [+]第一版

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,98 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode'
+import { intl } from 'ext-intl'
+import * as fs from 'fs'
+import * as path from 'path'
+import * as prettier from 'prettier'
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "ext-intl" is now active!')
+export interface IConfig {
+  outputPath: string
+  rootPath: string
+  template: boolean
+  extractOnly: boolean
+  whiteList: string[]
+  mode?: 'sample' | 'depth' // 模式类型 简单模式/深层次导出
+  prefix?: string[]
+  // 用于处理模板字符串的配置
+  templateString?: {
+    funcName: string
+  }
+}
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand('ext-intl.intl', () => {
-    // The code you place here will be executed every time your command is executed
+function resolvePath(pathName: string) {
+  return path.resolve(vscode.workspace.rootPath!, pathName)
+}
 
-    // Display a message box to the user
-    vscode.window.showInformationMessage('Hello World from ext-intl!')
+export const INIT_CONFIG: IConfig = {
+  outputPath: resolvePath('./intl'),
+  rootPath: resolvePath('./src'),
+  template: false,
+  extractOnly: true,
+  whiteList: ['.ts', '.tsx', '.js', '.jsx'],
+  mode: 'depth',
+  prefix: [],
+  templateString: {
+    funcName: 'intl.get',
+  },
+}
+
+function checkConfig(dir: string = '', fileName: string) {
+  return new Promise((resolve, reject) => {
+    if (!dir) {
+      reject('当前不存在`workspace`，请先打开工作空间后再运行')
+    }
+    const configPath = path.resolve(dir, fileName)
+    fs.readFile(configPath, { encoding: 'utf-8' }, (err, data) => {
+      if (err) {
+        reject(err.message)
+      }
+      const config = JSON.parse(data)
+      resolve(config)
+    })
   })
+}
 
-  context.subscriptions.push(disposable)
+function initConfig(configPath: string) {
+  if (!fs.existsSync(configPath)) {
+    fs.writeFileSync(
+      configPath,
+      prettier.format(JSON.stringify(INIT_CONFIG), { parser: 'json' })
+    )
+  }
+}
+
+function generateStatusBar(text: string, command: string) {
+  const btn = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left)
+  btn.text = text
+  btn.command = command
+  btn.show()
+}
+
+const CONFIG_FILE_NAME = '.extintl.json'
+
+export function activate(context: vscode.ExtensionContext) {
+  generateStatusBar('init', 'ext-intl:init')
+  generateStatusBar('generate', 'ext-intl:generate')
+
+  let disposable = vscode.commands.registerCommand('ext-intl:generate', () => {
+    checkConfig(vscode.workspace.rootPath, CONFIG_FILE_NAME)
+      .then((config) => {
+        intl(config as any)
+      })
+      .catch((err) => {
+        vscode.window.showErrorMessage(err)
+      })
+  })
+  const init = vscode.commands.registerCommand('ext-intl:init', () => {
+    if (!vscode.workspace.rootPath) {
+      vscode.window.showErrorMessage(
+        '当前不存在`workspace`，请先打开工作空间后再运行'
+      )
+      return
+    }
+    initConfig(resolvePath(CONFIG_FILE_NAME))
+    vscode.window.showInformationMessage('初始化成功')
+  })
+  context.subscriptions.push(disposable, init)
 }
 
 // this method is called when your extension is deactivated
